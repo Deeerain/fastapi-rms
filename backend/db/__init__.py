2c1c73b97cc8b89d29d156f0ff0c4d2f2f651a9f
deerain <d33.i2.1n@gmail.com>
2023-07-06 13:08:52 +0300

    moved access

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -1,8 +1,6 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import declarative_base, sessionmaker
+from db.connection import get_session, Base
 
-engine = create_async_engine('postgresql+asyncpg://scott:tiger@localhost/test')
-
-Base = declarative_base()
-
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+__all__ = [
+    'get_session',
+    'Base',
+]
